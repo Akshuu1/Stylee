0d419015778433b1993e49d8d84a77a1dc3fb420
Akshuu1 <akshata.14feb@gmail.com>
2025-07-28 23:55:15 +0530

    Changed dimension

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -25,20 +25,20 @@ const Featured = () => {
                 <motion.div 
                 onHoverStart={()=>handleHolver(0)} 
                 onHoverEnd={()=>handleHoverEnd(0)}
-                className="cardContainer w-1/2 relative ">
+                className="cardContainer w-1/2 mr-15 relative ">
                     <h1  className="absolute flex overflow-hidden text-[#CDEA68] z-[9] -mb-10 top-1/2 left-full -translate-x-1/2 -translate-y-1/2 leading-none tracking-right text-8xl">
                         {"Runway Trends".split('').map((item,index) => 
                         <motion.span initial={{y:"100%"}} transition={{ease:[0.22,1,0.36,1],delay:index*0.02}} animate={cards[0]} className='inline-block' key={index}>{item}</motion.span> 
                         )}
                     </h1>
-                    <div className="overflow-hidden w-full h-full  rounded-xl scale-90">
+                    <div className="overflow-hidden w-full h-full rounded-xl scale-90">
                         <img src="/photos/img2.jpg" className='hover:scale-95 duration-[1s]' alt="" />    
                     </div>   
                 </motion.div>
                 <motion.div 
                 onHoverStart={()=>handleHolver(1)} 
                 onHoverEnd={()=>handleHoverEnd(1)}
-                className="cardContainer w-1/2 relative  ">
+                className="cardContainer w-1/2 relative   ">
                     <h1 className="absolute flex overflow-hidden text-[#CDEA68] z-[9] -mb-10 top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 leading-none tracking-right text-8xl">
                         {"Street Style".split('').map((item,index) => 
                         <motion.span initial={{y:"100%"}} transition={{ease:[0.22,1,0.36,1],delay:index*0.02}} animate={cards[1]} className='inline-block' key={index}>{item}</motion.span> 
@@ -53,7 +53,7 @@ const Featured = () => {
                 <motion.div 
                 onHoverStart={()=>handleHolver(0)} 
                 onHoverEnd={()=>handleHoverEnd(0)}
-                className="cardContainer w-1/2 relative  ">
+                className="cardContainer w-1/2 relative mr-15  ">
                     <h1 className="absolute flex overflow-hidden text-[#CDEA68] z-[9] -mb-10 top-1/2 left-full -translate-x-1/2 -translate-y-1/2 leading-none tracking-right text-8xl">
                         {"Manish Malhotra's".split('').map((item,index) => 
                         <motion.span initial={{y:"100%"}} transition={{ease:[0.22,1,0.36,1],delay:index*0.02}} animate={cards[0]} className='inline-block' key={index}>{item}</motion.span> 
@@ -81,7 +81,7 @@ const Featured = () => {
                 <motion.div 
                 onHoverStart={()=>handleHolver(0)} 
                 onHoverEnd={()=>handleHoverEnd(0)}
-                className="cardContainer w-1/2 relative  ">
+                className="cardContainer w-1/2 relative mr-15  ">
                     <h1 className="absolute flex overflow-hidden text-[#CDEA68] z-[9] -mb-10 top-1/2 left-full -translate-x-1/2 -translate-y-1/2 leading-none tracking-right text-8xl">
                         {"Future Fashion".split('').map((item,index) => 
                         <motion.span initial={{y:"100%"}} transition={{ease:[0.22,1,0.36,1],delay:index*0.02}} animate={cards[0]} className='inline-block' key={index}>{item}</motion.span> 
@@ -109,7 +109,7 @@ const Featured = () => {
                 <motion.div 
                 onHoverStart={()=>handleHolver(0)} 
                 onHoverEnd={()=>handleHoverEnd(0)}
-                className="cardContainer w-1/2 relative  ">
+                className="cardContainer w-1/2 relative mr-15 ">
                     <h1 className="absolute flex overflow-hidden text-[#CDEA68] z-[9] -mb-10 top-1/2 left-full -translate-x-1/2 -translate-y-1/2 leading-none tracking-right text-8xl">
                         {"Recycled Couture".split('').map((item,index) => 
                         <motion.span initial={{y:"100%"}} transition={{ease:[0.22,1,0.36,1],delay:index*0.02}} animate={cards[0]} className='inline-block' key={index}>{item}</motion.span> 
