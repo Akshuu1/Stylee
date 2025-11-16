6c37221ea91bd26939dc008b301f380ffb30d369
Akshuu1 <akshata.14feb@gmail.com>
2025-11-17 01:41:19 +0530

    added backend link

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -10,7 +10,7 @@ export default function Profile() {
 
     // 2️⃣ Make a GET request to the protected backend route
     axios
-      .get("http://localhost:5000/api/profile", {
+      .get("https://stylee-4cbh.vercel.app/api/profile", {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((res) => setUser(res.data.user))
