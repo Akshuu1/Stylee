aea0e51f1d796f47ad56a437cf0ad52ec2d58588
Akshuu1 <akshata.14feb@gmail.com>
2025-11-17 16:03:49 +0530

    fixing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "[http://localhost:5173 , https://stylee-seven.vercel.app/]",
-    credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 
